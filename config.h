b4af4eec6bbbf209427500f3a4cd8cdfe5cdfacd
Alexander Neumann <alexander@bumpern.de>
2018-06-25 20:27:28 +0200

    Increase debounce timer
    https://github.com/qmk/qmk_firmware/issues/910

--- config.h
+++ config.h
@@ -55,7 +55,7 @@ Project located at <https://github.com/benblazak/ergodox-firmware>
  *
  * And so, there is no sense to have DEBOUNCE higher than 2.
  */
-#define DEBOUNCE        2
+#define DEBOUNCE        3
 #define TAPPING_TERM    230
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
